b782baca461d84ad4c68135f36c0f0cb534eb936
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-06-05 21:07:13 +0900

    一覧ページと詳細ページの作成

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
 from django.urls import path, include
+from .views import TaskList, TaskDetail
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', TaskList.as_view()),
+    path('detail/<int:pk>', TaskDetail.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Task
 
 # Create your views here.
+
+class TaskList(ListView):
+  template_name = 'list.html'
+  model = Task
+
+class TaskDetail(DetailView):
+  template_name = 'detail.html'
+  model = Task
