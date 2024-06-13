4ca5137fa0785c492965d6c3a93798cd091a8730
ryosuke2012 <117160982+ryosuke2012@users.noreply.github.com>
2024-06-13 22:22:31 +0900

    登録テストの実装

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,72 @@
 from django.test import TestCase
+from django.urls import reverse
+from .forms import TaskForm
+from .models import Task, Status
 
-# Create your tests here.
+class TaskFormTests(TestCase):
+  
+  def setUp(self):
+    self.status = Status.objects.create(status='未着手')
+
+  def test_valid_when_given_task_name_and_status_id(self):
+    data = {
+      'task_name': 'Test Task',
+      'status_id': self.status.id
+    }
+    task = Task()
+    form =TaskForm(data, instance=task)
+    self.assertTrue(form.is_valid())
+
+  def test_valid_when_not_given_start_date(self):
+    data = {
+      'task_name': 'Test Task',
+      'status_id': self.status.id
+    }
+    task = Task()
+    task.start_date = None
+    form =TaskForm(data, instance=task)
+    self.assertTrue(form.is_valid())
+
+  def test_valid_when_not_given_end_date(self):
+    data = {
+      'task_name': 'Test Task',
+      'status_id': self.status.id
+    }
+    task = Task()
+    task.end_date = None
+    form =TaskForm(data, instance=task)
+    self.assertTrue(form.is_valid())
+
+  def test_invalid_when_not_given_task_name(self):
+    data = {
+      'task_name': '',
+      'status_id': self.status.id
+    }
+    task = Task()
+    form =TaskForm(data, instance=task)
+    self.assertFalse(form.is_valid())
+
+  def test_invalid_when_task_name_null(self):
+    data = {
+      'status_id': self.status.id
+    }
+    task = Task()
+    form =TaskForm(data, instance=task)
+    self.assertFalse(form.is_valid())
+
+  def test_invalid_when_not_given_status_id(self):
+    data = {
+      'task_name': 'Test Task',
+      'status_id': ''
+    }
+    task = Task()
+    form =TaskForm(data, instance=task)
+    self.assertFalse(form.is_valid())
+
+  def test_invalid_when_status_id_null(self):
+    data = {
+      'task_name': 'Test Task'
+    }
+    task = Task()
+    form =TaskForm(data, instance=task)
+    self.assertFalse(form.is_valid())
